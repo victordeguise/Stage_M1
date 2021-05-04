3c2222c21268555d092dc53b8887fa4ebe925f6e
victordeguise <64610744+victordeguise@users.noreply.github.com>
2021-05-04 10:09:08 +0200

    Update pipeline.r

--- pipeline.r
+++ pipeline.r
@@ -18,12 +18,15 @@ dir= "C:/Users/Victor/Desktop/Cours/Master/S2/Stage/Projet/"
 #Install rBlast from GitHub using devtools::install_github("mhahsler/rBLAST").
 
 
+## Pour avoir accès a la commande makeblastdb et blast, il faut situer l'emplacement du dossier qui contient l'executable
+## A télécharger ici : https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ (en fonction du système d'exploitation)
 
 Sys.setenv(PATH = paste(Sys.getenv("PATH"), "C:\\Users\\Victor\\Desktop\\Cours\\Master\\S2\\Stage\\Projet\\ncbi-blast-2.11.0+\\bin",
                         sep= .Platform$path.sep))
+
 library(rBLAST)
 
-### Cr?ation des bases de donn?es avec nos transcriptomes femelle et male ###
+### Création des bases de données avec nos transcriptomes femelle et male ###
 
 makeblastdb(file="Donnees/Transcriptomes/highest_iso_GFBF_GHCZ01.1.fsa_nt", dbtype = "nucl", args = c("-out GfossB_f -title GfossB_f -parse_seqids"))
 makeblastdb(file="Donnees/Transcriptomes/highest_iso_GFBM_GHDA01.1.fsa_nt", dbtype = "nucl", args=c("-out GfossB_m -title GfossB_m -parse_seqids"))
@@ -35,196 +38,157 @@ blast_m <- blast(db="GfossB_m", type= "tblastn")
 
 ### Affiliation des fichiers fasta ###
 
-cyp302 <- readAAStringSet("Genes_Biosynthesis/CYP302A1_Hya.fasta", format = "fasta")
-cyp306 <- readAAStringSet("Genes_Biosynthesis/CYP306D1_Hya.fasta", format = "fasta")
-cyp307 <- readAAStringSet("Genes_Biosynthesis/CYP307A2_Hya.fasta", format = "fasta")
-cyp314 <- readAAStringSet("Genes_Biosynthesis/CYP314A1_Hya.fasta", format = "fasta")
-cyp315 <- readAAStringSet("Genes_Biosynthesis/CYP315A1_Hya.fasta", format = "fasta")
+list_cyp_Hya <- c("CYP18H1", "CYP302A1", "CYP306D1", "CYP307A2", "CYP314A1", "CYP315A1")
+fasta_cyp_Hya <- vector(mode="list", length=6)
+names(fasta_cyp_Hya) <- c("CYP18H1", "CYP302A1", "CYP306D1", "CYP307A2", "CYP314A1", "CYP315A1")
 
-### Blast contre le transcriptome femelle ###
+for ( i in 1:length(list_cyp_Hya) ){
+  
+  fasta_cyp_Hya[[i]] <- readAAStringSet(paste("Genes_Biosynthesis/",list_cyp_Hya[i],"_Hya.fasta", sep =""), format = "fasta")
+  
+}
 
-cyp302_blast_f <- predict(blast_f, cyp302)
-cyp306_blast_f <- predict(blast_f, cyp306)
-cyp307_blast_f <- predict(blast_f, cyp307)
-cyp314_blast_f <- predict(blast_f, cyp314)
-cyp315_blast_f <- predict(blast_f, cyp315)
+### Blast contre le transcriptome femelle/male ###
 
-
-### Blast contre le transcriptome male ###
-
-cyp302_blast_m <- predict(blast_m, cyp302)
-cyp306_blast_m <- predict(blast_m, cyp306)
-cyp307_blast_m <- predict(blast_m, cyp307)
-cyp314_blast_m <- predict(blast_m, cyp314)
-cyp315_blast_m <- predict(blast_m, cyp315)
+cyp_blast_f <- vector(mode="list", length=6)
+cyp_blast_m <- vector(mode="list", length=6)
+names(cyp_blast_f) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
+names(cyp_blast_m) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
 
 
-### Top 1 r�sultats Blast femelle ###
+for ( i in 1:length(list_cyp_Hya) ){
+  
+  cyp_blast_f[[i]] <- predict(blast_f, fasta_cyp_Hya[[i]])
+  cyp_blast_m[[i]] <- predict(blast_m, fasta_cyp_Hya[[i]])
+  
+}
 
-top_cyp302_blast_f <- cyp302_blast_f[1,]
-top_cyp306_blast_f <- cyp306_blast_f[1,]
-top_cyp307_blast_f <- cyp307_blast_f[1,]
-top_cyp314_blast_f <- cyp314_blast_f[1,]
-top_cyp315_blast_f <- cyp315_blast_f[1,]
+### Top 1 résultats Blast femelle/male ###
 
+top_cyp_blast_f <- vector(mode="list", length=6)
+top_cyp_blast_m <- vector(mode="list", length=6)
+names(top_cyp_blast_f) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
+names(top_cyp_blast_m) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
 
-### Top 1 r?sultats Blast male ###
+for ( i in 1:length(list_cyp_Hya) ){
+  
+  top_cyp_blast_f[[i]] <- cyp_blast_f[[i]][1,]
+  top_cyp_blast_m[[i]] <- cyp_blast_m[[i]][1,]
+  
+}
 
-top_cyp302_blast_m <- cyp302_blast_m[1,]
-top_cyp306_blast_m <- cyp306_blast_m[1,]
-top_cyp307_blast_m <- cyp307_blast_m[1,]
-top_cyp314_blast_m <- cyp314_blast_m[1,]
-top_cyp315_blast_m <- cyp315_blast_m[1,]
-
-
-### Assignation du fasta ? partir de l'id du transcriptome ###
-
-### pour la femelle ###
+### Assignation du fasta à partir de l'id du transcriptome ###
 
 library(seqinr)
 # install.packages(seqinr)
+
 transcriptome_f <- read.fasta("Donnees/Transcriptomes/highest_iso_GFBF_GHCZ01.1.fsa_nt")
-
-fasta_cyp302_blast_f <- transcriptome_f[top_cyp302_blast_f$SubjectID]
-fasta_cyp306_blast_f <- transcriptome_f[top_cyp306_blast_f$SubjectID]
-fasta_cyp307_blast_f <- transcriptome_f[top_cyp307_blast_f$SubjectID]
-fasta_cyp314_blast_f <- transcriptome_f[top_cyp314_blast_f$SubjectID]
-fasta_cyp315_blast_f <- transcriptome_f[top_cyp315_blast_f$SubjectID]
-
-
-### pour le male ### 
 transcriptome_m <- read.fasta("Donnees/Transcriptomes/highest_iso_GFBM_GHDA01.1.fsa_nt")
 
-fasta_cyp302_blast_m <- transcriptome_m[top_cyp302_blast_m$SubjectID]
-fasta_cyp306_blast_m <- transcriptome_m[top_cyp306_blast_m$SubjectID]
-fasta_cyp307_blast_m <- transcriptome_m[top_cyp307_blast_m$SubjectID]
-fasta_cyp314_blast_m <- transcriptome_m[top_cyp314_blast_m$SubjectID]
-fasta_cyp315_blast_m <- transcriptome_m[top_cyp315_blast_m$SubjectID]
+fasta_cyp_blast_f <- vector(mode="list", length=6)
+fasta_cyp_blast_m <- vector(mode="list", length=6)
+names(fasta_cyp_blast_f) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
+names(fasta_cyp_blast_m) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
+
+for ( i in 1:length(list_cyp_Hya) ){
+  
+  fasta_cyp_blast_f[[i]] <- transcriptome_f[top_cyp_blast_f[[i]]$SubjectID]
+  fasta_cyp_blast_m[[i]] <- transcriptome_m[top_cyp_blast_m[[i]]$SubjectID]
+  
+}
 
 
-### Recherche d'ORFs ###
+### Recherche d'ORFs chez le male et la femelle et écriture dans un répertoire ###
 
 #install.packages("LncFinder")
 library(LncFinder)
 
-# ORF chez la femelle
+list_cyp <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
+orf_dna_f <- vector(mode="list", length=6)
+orf_dna_m <- vector(mode="list", length=6)
+orf_Forward_aa_cyp_f = list()
+orf_Forward_aa_cyp_m = list()
+names(orf_dna_f) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
+names(orf_dna_m) <- c("CYP18", "CYP302", "CYP306", "CYP307", "CYP314", "CYP315")
 
-orf_dna_cyp302_f <-find_orfs(fasta_cyp302_blast_f,reverse.strand = TRUE)
-orf_Forward_aa_cyp302_f <- paste(translate(s2c(orf_dna_cyp302_f$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp302_f <- paste(translate(s2c(orf_dna_cyp302_f$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp306_f <-find_orfs(fasta_cyp306_blast_f,reverse.strand = TRUE )
-orf_Forward_aa_cyp306_f <- paste(translate(s2c(orf_dna_cyp306_f$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp306_f <- paste(translate(s2c(orf_dna_cyp306_f$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp307_f <-find_orfs(fasta_cyp307_blast_f,reverse.strand = TRUE )
-orf_Forward_aa_cyp307_f <- paste(translate(s2c(orf_dna_cyp307_f$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp307_f <- paste(translate(s2c(orf_dna_cyp307_f$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp314_f <-find_orfs(fasta_cyp314_blast_f,reverse.strand = TRUE )
-orf_Forward_aa_cyp314_f <- paste(translate(s2c(orf_dna_cyp314_f$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp314_f <- paste(translate(s2c(orf_dna_cyp314_f$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp315_f <-find_orfs(fasta_cyp315_blast_f,reverse.strand = TRUE )
-orf_Forward_aa_cyp315_f <- paste(translate(s2c(orf_dna_cyp315_f$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp315_f <- paste(translate(s2c(orf_dna_cyp315_f$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-# ORF chez le male
-
-orf_dna_cyp302_m <-find_orfs(fasta_cyp302_blast_m,reverse.strand = TRUE )
-orf_Forward_aa_cyp302_m <- paste(translate(s2c(orf_dna_cyp302_m$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp302_m <- paste(translate(s2c(orf_dna_cyp302_m$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp306_m <-find_orfs(fasta_cyp306_blast_m,reverse.strand = TRUE )
-orf_Forward_aa_cyp306_m <- paste(translate(s2c(orf_dna_cyp306_m$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp306_m <- paste(translate(s2c(orf_dna_cyp306_m$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp307_m <-find_orfs(fasta_cyp307_blast_m,reverse.strand = TRUE )
-orf_Forward_aa_cyp307_m <- paste(translate(s2c(orf_dna_cyp307_m$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp307_m <- paste(translate(s2c(orf_dna_cyp307_m$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp314_m <-find_orfs(fasta_cyp314_blast_m,reverse.strand = TRUE )
-orf_Forward_aa_cyp314_m <- paste(translate(s2c(orf_dna_cyp314_m$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp314_m <- paste(translate(s2c(orf_dna_cyp314_m$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-orf_dna_cyp315_m <-find_orfs(fasta_cyp315_blast_m,reverse.strand = TRUE )
-orf_Forward_aa_cyp315_m <- paste(translate(s2c(orf_dna_cyp315_m$ORF.Forward$ORF.Max.Seq)), collapse = "")
-orf_Reverse_aa_cyp315_m <- paste(translate(s2c(orf_dna_cyp315_m$ORF.Reverse$ORF.Max.Seq)), collapse = "")
-
-### Ecriture des r?sultats dans le dossier R_Orf ###
-
-### pour la femelle
-### Forward strand
-
-write.fasta(orf_Forward_aa_cyp302_f, names = top_cyp302_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp302_f.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp306_f, names = top_cyp306_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp306_f.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp307_f, names = top_cyp307_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp307_f.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp314_f, names = top_cyp314_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp314_f.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp315_f, names = top_cyp315_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp315_f.fasta", open = "w")
-
-### Reverse strand
-
-write.fasta(orf_Reverse_aa_cyp302_f, names = top_cyp302_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp302_f.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp306_f, names = top_cyp306_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp306_f.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp307_f, names = top_cyp307_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp307_f.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp314_f, names = top_cyp314_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp314_f.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp315_f, names = top_cyp315_blast_f$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp315_f.fasta", open = "w")
+for ( i in 1:length(list_cyp) ){
+  
+  orf_dna_f[[i]] <- find_orfs(fasta_cyp_blast_f[[i]],reverse.strand = FALSE)
+  orf_dna_m[[i]] <- find_orfs(fasta_cyp_blast_m[[i]],reverse.strand = FALSE)
+  orf_Forward_aa_cyp_f[[i]] <- paste(translate(s2c(orf_dna_f[[i]]$ORF.Max.Seq)), collapse = "")
+  orf_Forward_aa_cyp_m[[i]] <- paste(translate(s2c(orf_dna_m[[i]]$ORF.Max.Seq)), collapse = "")
+  # Ecriture des résultats dans le dossier R_Orf/
+  write.fasta(orf_Forward_aa_cyp_f[[i]], names = top_cyp_blast_f[[i]]$SubjectID, 
+              file.out = paste("R_Orf/orf_Forward_",list_cyp[i],"_f.fasta", sep=""), open = "w")
+  write.fasta(orf_Forward_aa_cyp_m[[i]], names = top_cyp_blast_m[[i]]$SubjectID, 
+              file.out = paste("R_Orf/orf_Forward_",list_cyp[i],"_m.fasta", sep=""), open = "w")
+}
 
 
-
-### pour le male
-## Forward strand 
-
-write.fasta(orf_Forward_aa_cyp302_m, names = top_cyp302_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp302_m.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp306_m, names = top_cyp306_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp306_m.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp307_m, names = top_cyp307_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp307_m.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp314_m, names = top_cyp314_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp314_m.fasta", open = "w")
-write.fasta(orf_Forward_aa_cyp315_m, names = top_cyp315_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_forward_cyp315_m.fasta", open = "w")
-
-### Reverse strand
-
-write.fasta(orf_Reverse_aa_cyp302_m, names = top_cyp302_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp302_m.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp306_m, names = top_cyp306_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp306_m.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp307_m, names = top_cyp307_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp307_m.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp314_m, names = top_cyp314_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp314_m.fasta", open = "w")
-write.fasta(orf_Reverse_aa_cyp315_m, names = top_cyp315_blast_m$SubjectID, 
-            file.out = "R_Orf/orf_Reverse_cyp315_m.fasta", open = "w")
-
-
-### Alignement multiple pour la phylog?nie ###
+### Alignement multiple pour la phylogénie ###
 
 #if (!requireNamespace("BiocManager", quietly=TRUE))
 #  install.packages("BiocManager")
 #BiocManager::install("msa")
 library(msa)
+library(ape)
 
-msaClustalW(inputSeqs ="Phylogeny/Arthropod phylogeny.fasta", type = "protein")
+alignement1 <- readAAStringSet("Phylogeny/Arthropod_phylogeny.fasta")
+alignement2 <- readAAStringSet("R_Orf/orf_forward_cyp302_f.fasta")
+alignement_multiple <- paste(alignement1, alignement2)
+
+multiple_alignement <- msaMuscle(alignement_multiple, type = "protein")
+
+## Si on veut garder l'alignement multiple : 
+#write.phylip(multiple_alignement, "Phylogeny/Multiple_alignement.txt")
+
+#msaPrettyPrint(multiple_alignement, output = "pdf")
 
 
-### Phylog�nie ###
 
-#install.packages("phylogram")
-library(phylogram)
+### Phylogénie ###
+
+multiple_alignement <- msaConvert(multiple_alignement,type = "seqinr::alignment")
+d <- dist.alignment(multiple_alignement, "identity")
+tree <- nj(d)
+tree <- makeLabel(tree, space = "")
+plot.phylo(tree,type = "phylogram", main="Phylogenetic Tree", use.edge.length = FALSE, font = 2 )
 
 
 
+
+
+
+### Récupérer les 10 top blast Hits du cyp306 puis rechercher les ORFs  
+
+top_cyp306_blast_f <- cyp306_blast_f[1:10,]
+top_cyp306_blast_m <- cyp306_blast_m[1:10,]
+fasta_cyp306_blast_f <- transcriptome_f[top_cyp306_blast_f$SubjectID]
+fasta_cyp306_blast_m <- transcriptome_m[top_cyp306_blast_m$SubjectID]
+
+
+
+
+for ( i in 1:10 ) { 
+
+  orf_dna_cyp306_f[[i]] <- find_orfs(fasta_cyp306_blast_f[[i]], reverse.strand = TRUE)
+  orf_Forward_aa_cyp306_f[[i]] <-  paste(translate(s2c(orf_dna_cyp306_f[[i]]$ORF.Forward$ORF.Max.Seq)), collapse = "")
+  write.fasta(orf_Forward_aa_cyp306_f[[i]], names = top_cyp306_blast_f[i,]$SubjectID, 
+              file.out = paste("R_Orf/orf_forward_cyp306_f", i,".fasta", sep=""), open = "w")
+  orf_Reverse_aa_cyp306_f[[i]] <-  paste(translate(s2c(orf_dna_cyp306_f[[i]]$ORF.Reverse$ORF.Max.Seq)), collapse = "")
+  write.fasta(orf_Reverse_aa_cyp306_f[[i]], names = top_cyp306_blast_f[i,]$SubjectID, 
+              file.out = paste("R_Orf/orf_Reverse_cyp306_f", i,".fasta", sep=""), open = "w")
+}
+
+
+for ( i in 1:10 ) { 
+  
+  orf_dna_cyp306_m[[i]] <- find_orfs(fasta_cyp306_blast_m[[i]], reverse.strand = TRUE)
+  orf_Forward_aa_cyp306_m[[i]] <-  paste(translate(s2c(orf_dna_cyp306_m[[i]]$ORF.Forward$ORF.Max.Seq)), collapse = "")
+  write.fasta(orf_Forward_aa_cyp306_m[[i]], names = top_cyp306_blast_m[i,]$SubjectID, 
+              file.out = paste("R_Orf/orf_forward_cyp306_m", i,".fasta", sep=""), open = "w")
+  orf_Reverse_aa_cyp306_m[[i]] <-  paste(translate(s2c(orf_dna_cyp306_m[[i]]$ORF.Reverse$ORF.Max.Seq)), collapse = "")
+  write.fasta(orf_Reverse_aa_cyp306_m[[i]], names = top_cyp306_blast_m[i,]$SubjectID, 
+              file.out = paste("R_Orf/orf_Reverse_cyp306_m", i,".fasta", sep=""), open = "w")
+}
 
