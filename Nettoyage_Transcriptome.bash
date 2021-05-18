7c5a5b726b3f9af47414b4664efcd6cb29a640e1
victordeguise <64610744+victordeguise@users.noreply.github.com>
2021-05-18 16:16:02 +0200

    Update Nettoyage_Transcriptome.bash

--- Nettoyage_Transcriptome.bash
+++ Nettoyage_Transcriptome.bash
@@ -2,6 +2,9 @@
 # Deguise Victor
 # 12/05/2021
 
+# To execute the script : ./Nettoyage_Transcriptome.bash assemblie_file R1 R2
+# Don't forget R1/R2 not already create
+# Refer to line 48 to know ther names. 
 
 # DIRECTORIES
 
@@ -24,7 +27,7 @@ fi
 # Rename Headers for all assemblies fasta
 
 while true; do
-	read -p "Do you want to rename headers for assemblies fasta ?" yn
+	read -p "Do you want to rename headers for assemblies fasta ?" yn 
 	case $yn in
 		[Yy]* ) for i in $(ls $ASSEMBLIES_DIR); do 
 			sed -i "s/\(^>\).* \(.*_DN[0-9]*_c[0-9]*_g[0-9]*_i[0-9]*\).*/\1\2/g" $ASSEMBLIES_DIR/${i}
@@ -36,24 +39,24 @@ done
 
 
 # Concat Fastq
-
 for i in $(ls $FASTQ_DIR); do
-	for j in $(echo ${i} | cut -d '_' -f4 | cut -d '.' -f1 ); do
-		if [ ! -f $FASTQ_DIR/${i::4}${j}.fastq ];
+	for j in $(echo ${i} | cut -d '_' -f4 | cut -d '.' -f1 ); do # cut permet d'extraire des caractères, ici on garde le R1 ou R2.
+		if [ ! -f $FASTQ_DIR/${i::4}${j}.fastq ]; # si le fichier n'existe pas alors on le créer.
 			then
 				echo "### Concatening Files" 
-				zcat $FASTQ_DIR/${i::-12}*${j}.fastq.gz >>$FASTQ_DIR/${i::4}${j}.fastq
+				zcat $FASTQ_DIR/${i::-12}*${j}.fastq.gz >>$FASTQ_DIR/${i::4}${j}.fastq # i::-12 pour enlever les 12 derniers caractères
+				# Le fichier prendra les 4 premiers caractères du fastq lu, le R1 ou R2 puis .fastq
 			fi
 	done
-	#rm ${i} # suppression du Fastq compressé, sauf si on veut garder le fichier ?
+	#rm $FASTQ_DIR/${i} # suppression du Fastq compressé
 done
 
 
 ### CHOIX DES INPUTS ###
 
-my_transcript=$ASSEMBLIES_DIR/GPulex-f_GHCP01.1.fsa_nt
-R1=$FASTQ_DIR/C4F_R1.fastq
-R2=$FASTQ_DIR/C4F_R2.fastq
+my_transcript=$1
+R1=$2
+R2=$3
 
 # Abundance estimation 
 
@@ -61,7 +64,7 @@ $TRINITY/align_and_estimate_abundance.pl \
 --transcripts ${my_transcript} --seqType fq \
 --left ${R1} --right ${R2} --SS_lib_type RF --est_method RSEM \
 --aln_method bowtie --prep_reference --output_dir rsem_outdir/${my_transcript::8} --trinity_mode \
---thread_count 20		# On fait /${my_transcript::X} pour prendre que les X 1er caractères, ici "GPulex-f"
+--thread_count 20		# On fait ${my_transcript::X} pour prendre que les X 1er caractères
 
 echo "### First step end"
 
@@ -80,3 +83,9 @@ $TRINITY/filter_low_expr_transcripts.pl \
 --highest_iso_only --trinity_mode >$RESULTS/highest_iso_${my_transcript}
 
 echo "### Finish !!"
+
+# On fait de la place 
+
+rm $MYDIR/RSEM.*
+rm $ASSEMBLIES_DIR/${my_transcript}.*
+rm -r $MYDIR/rsem_outdir/*
